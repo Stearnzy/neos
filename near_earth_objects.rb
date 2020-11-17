6fde7b37681996df99df1fb4a80174b9209e63b1
Zach Stearns <zach.stearns92@gmail.com>
2020-11-17 16:06:11 -0700

    Change test to pass

--- near_earth_objects.rb
+++ near_earth_objects.rb
@@ -1,6 +1,7 @@
 require 'faraday'
 require 'figaro'
 require 'pry'
+require 'json'
 # Load ENV vars via Figaro
 Figaro.application = Figaro::Application.new(environment: 'production', path: File.expand_path('../config/application.yml', __FILE__))
 Figaro.load
@@ -12,9 +13,7 @@ class NearEarthObjects
       params: { start_date: date, api_key: ENV['nasa_api_key']}
     )
     asteroids_list_data = conn.get('/neo/rest/v1/feed')
-
     parsed_asteroids_data = JSON.parse(asteroids_list_data.body, symbolize_names: true)[:near_earth_objects][:"#{date}"]
-
     largest_astroid_diameter = parsed_asteroids_data.map do |astroid|
       astroid[:estimated_diameter][:feet][:estimated_diameter_max].to_i
     end.max { |a,b| a<=> b}
