9fba05ef219d3b3b13a95c144a6e3aed4c2e9bfe
Zach Stearns <zach.stearns92@gmail.com>
2020-11-20 17:55:36 -0700

    Rename file

---
+++ astroid.rb
@@ -0,0 +1,11 @@
+class Astroid
+    attr_reader :name, :diameter, :miss_distance
+
+    def initialize(data)
+        @name = data[:name]
+        # Diameter is a FLOAT
+        @diameter = data[:estimated_diameter][:feet][:estimated_diameter_max]
+        # Miss Distance is a STRING
+        @miss_distance = data[:close_approach_data][0][:miss_distance][:miles]
+    end
+end
