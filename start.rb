2a420a1dfcbfbe45c50e6bec94c8b6d3b5a6dae8
Zach Stearns <zach.stearns92@gmail.com>
2020-11-25 20:11:14 -0700

    Move welcome message

--- start.rb
+++ start.rb
@@ -1,11 +1,15 @@
 require 'pry'
 
 require_relative 'near_earth_objects'
+require_relative 'neos_view'
 
-puts "________________________________________________________________________________________________________________________________"
-puts "Welcome to NEO. Here you will find information about how many meteors, astroids, comets pass by the earth every day. \nEnter a date below to get a list of the objects that have passed by the earth on that day."
-puts "Please enter a date in the following format YYYY-MM-DD."
-print ">>"
+view = NeosView.new
+view.welcome_message
+
+# puts "________________________________________________________________________________________________________________________________"
+# puts "Welcome to NEO. Here you will find information about how many meteors, astroids, comets pass by the earth every day. \nEnter a date below to get a list of the objects that have passed by the earth on that day."
+# puts "Please enter a date in the following format YYYY-MM-DD."
+# print ">>"
 
 date = gets.chomp
 astroid_details = NearEarthObjects.find_neos_by_date(date)
