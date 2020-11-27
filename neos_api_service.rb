57b789eb937d36e18eca972e0c8f0fe6b1fb93ee
Zach Stearns <zach.stearns92@gmail.com>
2020-11-27 16:51:19 -0700

    Tidy and remove old code

--- neos_api_service.rb
+++ neos_api_service.rb
@@ -1,22 +1,23 @@
 class NeosApiService
-    def get_astroids(date)
-        response = retreive_data(date)
-        parse(response, date)
-    end
+  def get_astroids(date)
+    response = retreive_data(date)
+    parse(response, date)
+  end
 
-private
-    def access_api(date)
-        Faraday.new(
-         url: 'https://api.nasa.gov',
-         params: { start_date: date, api_key: ENV['nasa_api_key']}
-          )
-    end
+  private
 
-    def retreive_data(date)
-        access_api(date).get('/neo/rest/v1/feed')
-    end
+  def access_api(date)
+    Faraday.new(
+      url: 'https://api.nasa.gov',
+      params: { start_date: date, api_key: ENV['nasa_api_key'] }
+    )
+  end
 
-    def parse(response, date)
-        JSON.parse(response.body, symbolize_names: true)[:near_earth_objects][:"#{date}"]
-    end
+  def retreive_data(date)
+    access_api(date).get('/neo/rest/v1/feed')
+  end
+
+  def parse(response, date)
+    JSON.parse(response.body, symbolize_names: true)[:near_earth_objects][:"#{date}"]
+  end
 end
