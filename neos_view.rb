0283a68e0f981a25dac0f776ba8a682e3bae636d
Zach Stearns <zach.stearns92@gmail.com>
2020-11-27 16:48:23 -0700

    Clean up syntax and remove old code

--- neos_view.rb
+++ neos_view.rb
@@ -41,7 +41,7 @@ class NeosView
   end
 
   def create_rows(astroid_data, column_info)
-    rows = astroid_data.each { |astroid| format_row_data(astroid, column_info) }
+    astroid_data.each { |astroid| format_row_data(astroid, column_info) }
   end
 
   def formatted_date(date)
@@ -59,23 +59,3 @@ class NeosView
     puts divider
   end
 end
-
-# column_labels = { name: "Name", diameter: "Diameter", miss_distance: "Missed The Earth By:" }
-# column_data = column_labels.each_with_object({}) do |(col, label), hash|
-#     hash[col] = {
-#         label: label,
-#         width: [@astroid_list.map { |astroid| astroid[col].size }.max, label.size].max}
-# end
-
-# header = "| #{ column_data.map { |_,col| col[:label].ljust(col[:width]) }.join(' | ') } |"
-# divider = "+-#{column_data.map { |_,col| "-"*col[:width] }.join('-+-') }-+"
-
-#     formated_date = DateTime.parse(date).strftime("%A %b %d, %Y")
-#     puts "______________________________________________________________________________"
-#     puts "On #{formated_date}, there were #{total_number_of_astroids} objects that almost collided with the earth."
-#     puts "The largest of these was #{largest_astroid} ft. in diameter."
-#     puts "\nHere is a list of objects with details:"
-#     puts divider
-#     puts header
-#     create_rows(astroid_list, column_data)
-#     puts divider
