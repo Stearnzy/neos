4d9d76c0242d3b7aaaab3cb5fe69c43b151806f6
Zach Stearns <zach.stearns92@gmail.com>
2020-11-27 16:50:48 -0700

    Tidy and remove old code

--- astroid.rb
+++ astroid.rb
@@ -1,11 +1,9 @@
 class Astroid
-    attr_reader :name, :diameter, :miss_distance
+  attr_reader :name, :diameter, :miss_distance
 
-    def initialize(data)
-        @name = data[:name]
-        # Diameter is a FLOAT
-        @diameter = data[:estimated_diameter][:feet][:estimated_diameter_max]
-        # Miss Distance is a STRING
-        @miss_distance = data[:close_approach_data][0][:miss_distance][:miles]
-    end
+  def initialize(data)
+    @name = data[:name]
+    @diameter = data[:estimated_diameter][:feet][:estimated_diameter_max]
+    @miss_distance = data[:close_approach_data][0][:miss_distance][:miles]
+  end
 end
